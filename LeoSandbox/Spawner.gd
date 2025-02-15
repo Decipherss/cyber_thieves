2bba5338b5e31f1be450eb6494913155f09eb87f
LeonardStowasser <leonard.stowasser@htk.academy>
2025-02-15 20:03:15 +0100

    NPC added, NavMesh added, spawning and despawning added

---
+++ LeoSandbox/Spawner.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var npc_scene: PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.timeout.connect(spawn_npc)
+
+func spawn_npc():
+	if npc_scene:
+		var npc = npc_scene.instantiate()
+		add_child(npc)
+		npc.global_transform = global_transform
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ LeoSandbox/leo_test_character.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+@onready var nav_agent = $NavigationAgent3D
+@export var speed: float = 3.0
+var target_location: Vector3
+
+func _ready() -> void:
+	if not nav_agent.velocity_computed.is_connected(_on_navigation_agent_3d_velocity_computed):
+		nav_agent.velocity_computed.connect(_on_navigation_agent_3d_velocity_computed)
+	
+	
+	choose_random_despawn_zone()
+	nav_agent.avoidance_enabled = false  # Verhindert komische Physikglitches
+
+func _physics_process(delta: float) -> void:
+	if nav_agent.is_navigation_finished():
+		return
+
+	# NAVIGATION: Korrekte Wegfindung aktivieren
+	var next_location = nav_agent.get_next_path_position()
+	next_location.y = global_transform.origin.y  # Höhe fixieren!
+	var _direction = (next_location - global_transform.origin).normalized()
+	velocity = _direction * speed
+	move_and_slide()
+
+	nav_agent.target_position = target_location
+
+func choose_random_despawn_zone():
+	var zones = get_tree().get_nodes_in_group("DespawnZone")
+	if zones.size() > 0:
+		target_location = zones.pick_random().global_transform.origin
+		target_location.y = global_transform.origin.y  # Höhe fixieren!
+		nav_agent.target_position = target_location
+		
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+	safe_velocity.y = 0  # Vertikale Bewegung blockieren!
+	velocity = velocity.move_toward(safe_velocity, 0.25)
+	move_and_slide()
